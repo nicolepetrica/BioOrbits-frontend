d8e58da41b3f9ba5cc5d9f008061354c624c4b07
HeitorHs <heiitorheenrique@gmail.com>
2025-10-05 23:50:01 -0300

    edit ips

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://3.135.179.15:5050",
+        target: "http://3.150.180.116:5050",
         changeOrigin: true,
       },
     },
